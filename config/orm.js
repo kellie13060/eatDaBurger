9e4ade3c167764052f54c21ac554cde76e658756
Kellie Hunsaker <kellie13060@gmail.com>
2020-01-31 12:49:02 -0700

    added all files

---
+++ config/orm.js
@@ -0,0 +1,31 @@
+var connection = require("../config/connection.js");
+
+
+
+var orm = {
+    selectAll: function (tableInput, cb) {
+        var queryString = "SELECT * FROM ?? ";
+        connection.query(queryString, [tableInput], function (err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+
+    insertOne: function (burgerName, cb) {
+        var queryString = "INSERT INTO burgers (burger_name) VALUES (?)";
+        connection.query(queryString, [burgerName], function (err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+
+    updateOne: function (devoured, burgerId, cb) {
+        var queryString = "UPDATE burgers SET devoured = ? WHERE id = ?";
+        connection.query(queryString, [devoured, burgerId], function (err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+};
+
+module.exports = orm;
---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require("express");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+//links the public directory and uses it. 
+app.use(express.static("public"));
+
+//parse body as JSON
+app.use(express.urlencoed({ expteded: true }));
+app.use(express.json());
+
+//set up handlbar
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}))
+app.set("veiw engine", "handlebars");
+
+//import routes and give server access
+var routes = require("./controllers/burgers_controllers.js");
+
+app.use(routes);
+
+//start server
+app.listen(PORT, function() {
+    //console log that the server has started
+    console.log("Server listening on: http://locailhost:" = PORT);
+});
